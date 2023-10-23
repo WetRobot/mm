05bb5a2bf983a2f539ee413c269530be092905f3
Joonas Miettinen <joonas.miettinen@gmail.com>
2023-10-23 20:54:06 +0300

    test: fix unit tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -3,11 +3,12 @@
 #include <cmath>
 
 TEST(MarkovModelTests, PositiveNumbers) {
-    mm::MarkovModel<std::string> M(1.0, 1.0, true);
+    mm::MarkovModel<std::string> M(1.0, 0.5, true);
     M.update("<<", "a");
     M.update("<<", "b");
     EXPECT_EQ(M.lpmf("<<", "a"), M.lpmf("<<", "b"));
-    EXPECT_EQ(M.lpmf("<<", "a"), std::log(1) - std::log(3));
+    EXPECT_EQ(M.lpmf("<<", "a"), std::log(2) - std::log(4.5));
+    EXPECT_EQ(M.lpmf("<<", "x"), std::log(0.5) - std::log(4.5));
 }
 
 int main(int argc, char **argv) {
